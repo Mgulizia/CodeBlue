34d0b4419641f533336b3e7505702c09240e3365
Mathew Gulizia <mathew.gulizia@blcsne.com>
2017-10-04 19:20:25 -0500

    Add project files.

---
+++ CodeBlue/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeBlue.Startup))]
+namespace CodeBlue
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
