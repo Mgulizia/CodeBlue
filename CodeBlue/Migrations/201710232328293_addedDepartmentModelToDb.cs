0b0c8e34a3f533a3fa1e9bb2ded43e5a59e2c42e
Mathew Gulizia <mathew.gulizia@blcsne.com>
2017-10-23 18:41:15 -0500

    added Department

---
+++ CodeBlue/Migrations/201710232328293_addedDepartmentModelToDb.cs
@@ -0,0 +1,26 @@
+namespace CodeBlue.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedDepartmentModelToDb : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        DepartmentName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Departments");
+        }
+    }
+}
