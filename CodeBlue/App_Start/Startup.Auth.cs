2425fc363af2f3fa76dc2a8c2f5d9fb76d924ffd
Mathew Gulizia <mathew.gulizia@blcsne.com>
2017-11-15 18:40:38 -0600

    Summary

--- CodeBlue/App_Start/Startup.Auth.cs
+++ CodeBlue/App_Start/Startup.Auth.cs
@@ -26,6 +26,7 @@ namespace CodeBlue
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
+                CookieName = "CoodoBuruu",
                 Provider = new CookieAuthenticationProvider
                 {
                     // Enables the application to validate the security stamp when the user logs in.
