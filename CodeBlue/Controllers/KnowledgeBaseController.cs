d54e47f81e788c0b86fa26782f32dfb4a8c2554c
Mark Renken <mark@timestonesoftware.com>
2017-11-27 19:27:53 -0600

    Add knowledgebase create to controller and a view

--- CodeBlue/Controllers/KnowledgeBaseController.cs
+++ CodeBlue/Controllers/KnowledgeBaseController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CodeBlue.Models;
+using CodeBlue.ViewModels.Accounts;
+using Microsoft.AspNet.Identity;
 
 namespace CodeBlue.Controllers
 {
     public class KnowledgeBaseController : Controller
     {
+        private ApplicationDbContext _context;
+
         // GET: KnowledgeBase
         public ActionResult Index()
         {
@@ -17,6 +22,29 @@ namespace CodeBlue.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(KnowledgeBase model)
+        {
+            _context = new ApplicationDbContext();
+
+            var title = model.ArticleTitle;
+            var article = model.Article;
+            var category = model.Category;
+            var dateAdded = DateTime.Today;
+            var userId = User.Identity.GetUserId();
+
+            var addArticle = new KnowledgeBase()
+            {
+                ArticleTitle = title,
+                Article = article,
+                Category = category,
+                DateAdded = dateAdded,
+                Userid = userId
+            };
+
+            return View("View");
         }
     }
 }
