8062a57455a41d9e61fba23cf87f3371c54ab654
tltillman <27322570+tltillman@users.noreply.github.com>
2017-11-29 19:23:11 -0600

    Controller unit tests

---
+++ CodeBlue.Tests/Controllers/KnowledgeBaseControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeBlue.Models;
+
+namespace CodeBlue.Tests.Controllers
+{
+    [TestClass]
+    public class KnowledgeBaseControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+
+        }
+
+        [TestMethod]
+        public void Create(KnowledgeBase model)
+        {
+
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+
+        }
+    }
+}
--- CodeBlue/Controllers/HomeController.cs
+++ CodeBlue/Controllers/HomeController.cs
@@ -12,20 +12,5 @@ namespace CodeBlue.Controllers
         {
             return View();
         }
-
-        //todo
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
